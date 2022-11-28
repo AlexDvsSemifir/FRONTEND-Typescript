db78ae98611b843c99e822aa9221cb2d769b246e
AlexDvsSemifir <alexandre.devos@semifir.com>
2022-11-28 10:30:18 +0100

    feat: correction exo 9 :sparkles:

---
+++ Exercice9.ts
@@ -0,0 +1,19 @@
+function monnaie(nombre: number): void {
+  const monnaies: number[] = [
+    500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01,
+  ];
+
+  for (let monnaie of monnaies) {
+    let nombreMonnaie = Math.floor(nombre / monnaie);
+    if (nombreMonnaie !== 0) {
+      if (monnaie >= 5) {
+        console.log(`Billet(s) de ${monnaie}€ : ${nombreMonnaie}`);
+      } else {
+        console.log(`Pièce(s) de ${monnaie}€ : ${nombreMonnaie}`);
+      }
+      nombre -= nombreMonnaie * monnaie;
+    }
+  }
+}
+
+monnaie(888.89);
